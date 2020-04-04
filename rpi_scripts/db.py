69b5cd1e0895ec9cd2047e95c58707f2318c41b6
Tomasz Gązwa <tomaszgazwa@MacBook-Pro-Tomasz-G.local>
2020-04-04 17:43:46 +0200

    Added raspberry pi scritps

---
+++ rpi_scripts/db.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys
+import Adafruit_DHT
+import mysql.connector
+import time
+from RPi import GPIO
+
+
+def db_add(temp, hum):
+
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="beep21",
+    passwd="password",
+    database="dht11"
+
+    )
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO `sensor`(`data`, `temp`, `hum`) VALUES (now(), %s, %s)"
+    val = (temp, hum)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+
+while True:
+    hum, temp = Adafruit_DHT.read_retry(11, 17)
+    db_add (temp, hum)
+    time.sleep(600)
+
---
+++ rpi_scripts/wateringON.py
@@ -0,0 +1,21 @@
+import RPi.GPIO  as GPIO
+import time
+
+channel = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.OUT)
+
+def podlewanie (pin):
+	GPIO.output(pin, GPIO.HIGH)
+
+
+def koniec (pin):
+	GPIO.output(pin, GPIO.LOW)
+
+
+if __name__ == '__main__':
+	try:
+		podlewanie(channel)
+	except KeyboardInterrupt:
+		GPIO.cleanup()
